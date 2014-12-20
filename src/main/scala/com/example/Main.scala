f00b866a825d60f592adf8e8ce46126e9b39940a
takashabe <takashabe.public@gmail.com>
2014-12-20 22:31:07 +0900

    テーブルの分割対応

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -3,10 +3,11 @@ package com.example
 import java.io.{IOException, File}
 import java.sql.{Connection, DriverManager, ResultSet}
 
-import net.gree.aurora.scala.application.AuroraShardingService
+import net.gree.aurora.scala.application.{AuroraTableNameService, AuroraShardingService}
 import net.gree.aurora.scala.domain.cluster.AbstractClusterIdResolver
 import net.gree.aurora.scala.domain.datasource.JDBCDataSource
 import net.gree.aurora.scala.domain.hint.Hint
+import net.gree.aurora.scala.domain.tablename.AbstractTableNameResolver
 import org.sisioh.dddbase.core.lifecycle.EntityNotFoundException
 import org.sisioh.dddbase.core.lifecycle.sync.SyncEntityIOContext
 
@@ -18,11 +19,12 @@ object Main extends App {
 
   // リゾルバを定義する
   private val clusterIdResolver = new AbstractClusterIdResolver[Int]("cluster") {
-
-    override protected def getSuffixName(userIdHint: Hint[Int], clusterSize: Int): String = {
-      (((userIdHint.value + 1) % clusterSize) + 1).toString
+    override protected def getSuffixName(userIdHint: Hint[Int], clusterSize: Int): String =  {
+      userIdHint.value % 10 match {
+        case n if n < 5 => "1"
+        case _          => "2"
+      }
     }
-
   }
 
   // AuroraShardingServiceを初期化する
@@ -31,7 +33,18 @@ object Main extends App {
   private implicit val dataSourceRepository = auroraShardingService.dataSourceRepository.get
   private implicit val ctx = SyncEntityIOContext
 
+  // テーブル名の解決
+  private val tableNameResolver = new AbstractTableNameResolver[Int]("user") {
+    override protected def getSuffixName(userIdHint: Int): String = {
+      "_" + "%d".format(userIdHint % 10)
+    }
+  }
+  private val auroraTableNameService = AuroraTableNameService(tableNameResolver, new File("./conf/application.conf"))
+
   private def findByUserId(userId: Int): Try[User] = {
+    // テーブル名を解決する
+    val table = auroraTableNameService.resolveByHint(userId).get
+
     // sharding-config-idとcluster-group-idとヒントを引数に与えてclusterを解決する
     auroraShardingService.resolveClusterByHint("database", "main", Hint(userId)).map { cluster =>
       // スレーブはランダム選択
@@ -41,14 +54,12 @@ object Main extends App {
       // 書き込み用途でマスターを利用する場合は、以下。
       // val dataSource = cluster.masterDataSource
 
-      println(dataSource)
-
       // データソースからコネクションを取得する
       val connection = getConnection(dataSource)
 
       // コネクションを使ってSQLを発行する
       val st = connection.createStatement
-      val rs = st.executeQuery("SELECT * FROM user WHERE id = " + userId)
+      val rs = st.executeQuery("SELECT * FROM " + table.name + " WHERE id = " + userId)
       if (rs.next())
         convertToModel(rs)
       else
@@ -70,7 +81,8 @@ object Main extends App {
       name = resultSet.getString("name")
     )
 
-  println(findByUserId(1).get)
-  println(findByUserId(2).get)
+  println(findByUserId(1000))
+  println(findByUserId(1055))
+  println(findByUserId(9999))
 
 }
